7e9a1ebb55d5f905884e25062f17efe650801967
theapache64 <theapache64@gmail.com>
2018-07-29 12:12:54 +0530

    First commit

---
+++ install.sh
@@ -0,0 +1,5 @@
+# Deleting default uglify.js file
+rm "~/.config/google-chrome/Default/Extensions/gidgenkbbabolejbgbpnhbimgjbffefm/1.9.3_0/uglify.js" &&
+
+# Downloading cracked ugilify.js file
+wget ""
